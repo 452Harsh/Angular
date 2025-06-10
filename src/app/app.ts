566a3eac2e9bdc6f0fcce218dccd4902c248b5da
452Harsh <harshtuli452@gmail.com>
2025-06-10 23:26:54 +0530

    event

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,23 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from './login/login';
-import { Profile } from './profile/profile';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Login,Profile],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  count = 0;
-  handleCount(val : string){
-    if(val === 'increment') {
-      this.count++;
-    } else if(val === 'decrement' && this.count>0) {
-      this.count--;
-    }else if(val === 'reset') {
-      this.count = 0;
-    }
+  handleEvent(event : Event) {
+    // Handle the mouse event here
+    console.log( event);
+    console.log("value" , (event.target as HTMLInputElement).value);
   }
 }
