787863edbffe1b28a462c456ba3d1d42352d3c62
452Harsh <harshtuli452@gmail.com>
2025-06-10 22:42:33 +0530

    counter app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login';
+import { Profile } from './profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Login,Profile],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'Angular';
+  count = 0;
+  handleCount(val : string){
+    if(val === 'increment') {
+      this.count++;
+    } else if(val === 'decrement' && this.count>0) {
+      this.count--;
+    }else if(val === 'reset') {
+      this.count = 0;
+    }
+  }
 }
---
+++ src/app/profile/profile.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'profile',
+  imports: [],
+//   template: `<h1>Profile Page</h1>`,
+  templateUrl: './profile.html',
+  styleUrls: ['./profile.css']
+           
+})
+export class Profile {
+
+}
