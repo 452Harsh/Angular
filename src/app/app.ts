3f1484c04b7700fcfe785e0419e36b4b1dd604db
452Harsh <harshtuli452@gmail.com>
2025-06-12 16:28:37 +0530

    signal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 
@@ -9,13 +9,29 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  users = ["alice", "bob", "charlie","bruce","dave","eve","frank","grace"];
-  studends = [
-    {name: "alice", age: 20},
-    {name: "bob", age: 22},
-    {name: "charlie", age: 21},
-    {name: "bruce", age: 23},
-    {name: "dave", age: 24},
-    {name: "eve", age: 22},     
-  ]
+data : WritableSignal<number | string> = signal(1);
+count : Signal<number> = signal(0); // it value cannot be changed
+counter = signal(0);
+x= signal(10);
+y = signal(20);
+z = computed(() => this.x() + this.y()); // computed signal that depends on x and y
+// computed signal that will update when x or y changes
+constructor() {
+  // Initialize the counter signal
+  effect(() => {
+    console.log('Counter value:', this.counter());
+  });
+}
+updatevalue() {
+  this.counter.update(value => value + 1);
+  console.log(this.counter());
+}
+updateSignal() {
+  this.data.set("hello"); // where as update signal will not work because it work only on single value
+}
+showValue() {
+  console.log(this.z()); // This will log the sum of x and y
+  this.y.set(80) ;  
+  console.log(this.z()); // This will log the updated sum of x and y
+}
 }
