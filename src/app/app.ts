bc39f4f17c3f49bab143d2e46ece270db85f009e
452Harsh <harshtuli452@gmail.com>
2025-06-12 23:16:54 +0530

    ng directives

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,19 @@
 import { Component, computed, effect, Signal, signal, WritableSignal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { NgIf ,NgFor} from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf, NgFor],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  bgColor ="red";
-  fs = "40px";
-  headingSizeBig = "80px";
-  headingSizeSmall = "40px";
-  zoom = !true;
-  updateHeadingSize() {
-    this.zoom = !this.zoom;
+  show = false;
+  students = ['alice', 'bob', 'charlie'];
+  toggleShow() {
+    this.show = !this.show;
   }
+
 }
